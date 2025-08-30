496c6c327f03c9ae0f752755c8ef5ec81ae3861a
praveendan <38735067+praveendan@users.noreply.github.com>
2025-08-30 09:28:24 -0500

    section 13 routeguard

--- section_13/src/main.js
+++ section_13/src/main.js
@@ -37,18 +37,40 @@ const router = createRouter({
       components: {
         default: UsersList,
         footer: UsersFooter
+      },
+      beforeEnter(to, from, next) {
+        console.log('users beforeEnter');
+        console.log(to, from);
+        next();
       }
     },
     { path: '/:notFound(.*)', component: NotFound }
   ],
   linkActiveClass: 'active',
-  scrollBehavior(to, from, savedPosition) {
-    console.log(to, from, savedPosition);
+  scrollBehavior(_, _2, savedPosition) {
+    // console.log(to, from, savedPosition);
     if (savedPosition) {
       return savedPosition;
     }
     return { left: 0, top: 0 };
   }
+});
+
+router.beforeEach(function(to, from, next) {
+  console.log('Global beforeEach');
+  console.log(to, from);
+  // if (to.name === 'team-members') {
+  //   next();
+  // } else {
+  //   next({ name: 'team-members', params: { teamId: 't2' } });
+  // }
+  next();
+});
+
+router.afterEach(function(to, from) {
+  // sending analytics data
+  console.log('Global afterEach');
+  console.log(to, from);
 });
 
 const app = createApp(App);
