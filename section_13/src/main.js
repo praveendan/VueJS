896cf91297f28e15894a5f3d2efbe91a4bcda4ec
praveendan <38735067+praveendan@users.noreply.github.com>
2025-08-30 09:40:50 -0500

    section 13 route metadata and file structure refactoring

--- section_13/src/main.js
+++ section_13/src/main.js
@@ -1,77 +1,7 @@
 import { createApp } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
-import TeamsList from './components/teams/TeamsList.vue';
-import UsersList from './components/users/UsersList.vue';
-import TeamMembers from './components/teams/TeamMembers.vue';
-import NotFound from './components/nav/NotFound.vue';
-import TeamsFooter from './components/teams/TeamsFooter.vue';
-import UsersFooter from './components/users/UsersFooter.vue';
-
-/**
- * Named routes will help to prevent code repition as we can assess the route via name
- * scrollBehavior can be used to auto scroll to the previous postion of the previous page when the user 
- * clicks back button
- */
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    { path: '/', redirect: '/teams' },
-    {
-      name: 'teams',
-      path: '/teams',
-      components: { default: TeamsList, footer: TeamsFooter },
-      children: [
-        {
-          name: 'team-members',
-          path: ':teamId',
-          component: TeamMembers,
-          props: true
-        } // /teams/t1
-      ]
-    }, // our-domain.com/teams => TeamsList
-    {
-      path: '/users',
-      components: {
-        default: UsersList,
-        footer: UsersFooter
-      },
-      beforeEnter(to, from, next) {
-        console.log('users beforeEnter');
-        console.log(to, from);
-        next();
-      }
-    },
-    { path: '/:notFound(.*)', component: NotFound }
-  ],
-  linkActiveClass: 'active',
-  scrollBehavior(_, _2, savedPosition) {
-    // console.log(to, from, savedPosition);
-    if (savedPosition) {
-      return savedPosition;
-    }
-    return { left: 0, top: 0 };
-  }
-});
-
-router.beforeEach(function(to, from, next) {
-  console.log('Global beforeEach');
-  console.log(to, from);
-  // if (to.name === 'team-members') {
-  //   next();
-  // } else {
-  //   next({ name: 'team-members', params: { teamId: 't2' } });
-  // }
-  next();
-});
-
-router.afterEach(function(to, from) {
-  // sending analytics data
-  console.log('Global afterEach');
-  console.log(to, from);
-});
+import router from './router.js';
 
 const app = createApp(App);
 
