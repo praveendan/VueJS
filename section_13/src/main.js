44c443a618e671851855b8e520ac73732b98e5c5
praveendan <38735067+praveendan@users.noreply.github.com>
2025-08-30 09:02:50 -0500

    section 13 nested routes

--- section_13/src/main.js
+++ section_13/src/main.js
@@ -11,9 +11,14 @@ const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/teams' },
-    { path: '/teams', component: TeamsList }, // our-domain.com/teams => TeamsList
+    {
+      path: '/teams',
+      component: TeamsList,
+      children: [
+        { path: ':teamId', component: TeamMembers, props: true },
+      ]
+    }, // our-domain.com/teams => TeamsList
     { path: '/users', component: UsersList },
-    { path: '/teams/:teamId', component: TeamMembers, props: true },
     { path: '/:notFound(.*)', component: NotFound }
   ],
   linkActiveClass: 'active'
