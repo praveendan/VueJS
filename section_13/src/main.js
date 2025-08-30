65abf2431d26c16370ef43315be7490b07e3c109
praveendan <38735067+praveendan@users.noreply.github.com>
2025-08-30 08:44:23 -0500

    section 13 dynamic paths and navigation

--- section_13/src/main.js
+++ section_13/src/main.js
@@ -4,6 +4,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
 
 const router = createRouter({
   /**
@@ -14,6 +15,7 @@ const router = createRouter({
   routes: [
     { path: '/teams', component: TeamsList }, // our-domain.com/teams => TeamsList
     { path: '/users', component: UsersList },
+    { path: '/teams/:teamId', component: TeamMembers },
   ],
   linkActiveClass: 'active'
 });
