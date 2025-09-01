bfc222309cbd39fb0f455205fb1d2abd20059c2e
praveendan <38735067+praveendan@users.noreply.github.com>
2025-09-01 08:51:39 -0500

    section 14 animated list init

--- section_14/src/App.vue
+++ section_14/src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="container">
+    <users-list></users-list>
+  </div>
+  <div class="container">
     <div class="block" :class="{animate: animatedBlock}"></div>
     <button @click="animateBlock">Animate</button>
   </div>
@@ -37,7 +40,12 @@
 </template>  
 
 <script>
+import UsersList from './components/UsersList.vue';
+
 export default {
+  components: {
+    UsersList,
+  },
   data() {
     return {
       animatedBlock: false,
@@ -45,7 +53,7 @@ export default {
       paraIsVisible: false,
       usersAreVisible: false,
       enterInterval: null,
-      leaveInterval: null
+      leaveInterval: null,
     };
   },
   methods: {
