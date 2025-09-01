bbda2859769e0511f524d73d390088bc9634f14c
praveendan <38735067+praveendan@users.noreply.github.com>
2025-09-01 08:30:40 -0500

    section 14 transition component with multiple children

--- section_14/src/App.vue
+++ section_14/src/App.vue
@@ -10,6 +10,13 @@
     </transition>
     <button @click="toggleParagraph">Toggle Paragraph</button>
   </div>
+  <div class="container">
+    <!-- out in will animate the removal and then the addition-->
+    <transition name="fade-button" mode="out-in">
+      <button @click="showUsers" v-if="!usersAreVisible">Show Users</button>
+      <button @click="hideUsers" v-else>Hide Users</button>
+    </transition>
+  </div>
   <base-modal @close="hideDialog" :open="dialogIsVisible">
     <p>This is a test dialog!</p>
     <button @click="hideDialog">Close it!</button>
@@ -26,9 +33,16 @@ export default {
       animatedBlock: false,
       dialogIsVisible: false,
       paraIsVisible: false,
+      usersAreVisible: false,
     };
   },
   methods: {
+    showUsers() {
+      this.usersAreVisible = true;
+    },
+    hideUsers() {
+      this.usersAreVisible = false;
+    },
     animateBlock() {
       this.animatedBlock = true;
     },
@@ -89,7 +103,7 @@ button:active {
 }
 .animate {
   /* transform: translateX(-150px); */
-  animation: slide-scale 0.3s ease-out forwards;
+  animation: slide-fade 0.3s ease-out forwards;
 }
 
 .para-enter-from {
@@ -119,6 +133,24 @@ button:active {
 .para-leave-to {
   /* opacity: 0;
   transform: translateY(30px); */
+}
+
+.fade-button-enter-from,
+.fade-button-leave-to {
+  opacity: 0;
+}
+
+.fade-button-enter-active {
+  transition: opacity 0.3s ease-out;
+}
+
+.fade-button-leave-active {
+  transition: opacity 0.3s ease-in;
+}
+
+.fade-button-enter-to,
+.fade-button-leave-from {
+  opacity: 1;
 }
 
 @keyframes slide-scale {
