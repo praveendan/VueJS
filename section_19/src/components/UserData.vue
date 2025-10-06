15c5248af9a8cd039dab60a081026d84426c1569
praveendan <38735067+praveendan@users.noreply.github.com>
2025-10-06 10:32:15 -0500

    section 19 provide inject in composition

--- section_19/src/components/UserData.vue
+++ section_19/src/components/UserData.vue
@@ -6,20 +6,22 @@
 </template>
 
 <script>
-import { computed } from 'vue';
+import { computed, inject } from 'vue';
 
 export default {
-  props: ['firstName', 'lastName', 'age'],
+  props: ['firstName', 'lastName'],
   setup(props, context) {
     const uName = computed(function () {
       return props.firstName + ' ' + props.lastName;
     });
 
+    const age = inject('userAge');
+
     console.log(context);
 
     // context.emit('save-data', 1); // this.$emit('save-data', 1);
 
-    return { userName: uName };
+    return { userName: uName, age };
   },
   // computed: {
   //   userName() {
