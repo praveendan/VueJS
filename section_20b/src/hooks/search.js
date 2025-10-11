59e4fc0394ee26f507c2269a85a286c9dc2e3f1e
praveendan <38735067+praveendan@users.noreply.github.com>
2025-10-11 09:51:45 -0500

    section 20b search Hook

--- section_20b/src/hooks/search.js
+++ section_20b/src/hooks/search.js
@@ -1,46 +1,36 @@
-import { ref, computed, watch, toRefs } from 'vue';
+import { ref, computed, watch } from 'vue';
 
 export default function useSearch(items, searchProp) {
   const enteredSearchTerm = ref('');
-    const activeSearchTerm = ref('');
+  const activeSearchTerm = ref('');
 
-    const availableItems = computed(function () {
-      if (activeSearchTerm.value) {
-        return items.filter((prj) =>
-          prj[searchProp].includes(activeSearchTerm.value)
-        );
+  const availableItems = computed(function() {
+    let filteredItems = [];
+    if (activeSearchTerm.value) {
+      filteredItems = items.value.filter(item =>
+        item[searchProp].includes(activeSearchTerm.value)
+      );
+    } else if (items.value) {
+      filteredItems = items.value;
+    }
+    return filteredItems;
+  });
+
+  watch(enteredSearchTerm, function(newValue) {
+    setTimeout(() => {
+      if (newValue === enteredSearchTerm.value) {
+        activeSearchTerm.value = newValue;
       }
-      return items;
-    });
+    }, 300);
+  });
 
-    const hasItems = computed(function () {
-      return items && availableItems.value.length > 0;
-    });
-
-    watch(enteredSearchTerm, function (newValue) {
-      setTimeout(() => {
-        if (newValue === enteredSearchTerm.value) {
-          activeSearchTerm.value = newValue;
-        }
-      }, 300);
-    });
-
-    // const propsWithRefs = toRefs(props);
-    // const user = propsWithRefs.user;
-
-    const { user } = toRefs(props);
-
-    watch(user, function () {
-      enteredSearchTerm.value = '';
-    });
-
-    function updateSearch(val) {
-      enteredSearchTerm.value = val;
+  function updateSearch(val) {
+    enteredSearchTerm.value = val;
   }
-  
+
   return {
-    updateSearch,
+    enteredSearchTerm,
     availableItems,
-    enteredSearchTerm
-  }
+    updateSearch
+  };
 }
